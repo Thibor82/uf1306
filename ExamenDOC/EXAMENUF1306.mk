7ce605f3419a855402ac59cb6af645a2eb4e150f
Ruben <ruben.caballero.lopez@gmail.com>
2024-09-10 09:39:20 +0200

    añado pdf y markdown con instrucciones

---
+++ ExamenDOC/EXAMENUF1306.mk
@@ -0,0 +1,22 @@
+### 📃 Examen UF1306 📃
+
+#Análisis, revisión y solución de errores en desarrollo web.
+___
+
+Utilizando la herramienta otorgada por el navegador Chrome “LIGHTHOUSE” detectar errores de rendimiento, carga, velocidad, etc… dentro del proyecto facilitado.
+
+
+
+1- Clonar el repositorio de GitHub: [Repo ExamenUF1306](https://github.com/Thibor82/uf1306)
+
+2- Probar, testear, detectar y anotar errores y posibles mejoras. (Por ejemplo: Redimensionamiento de las imágenes, posteo de mensajes, reescalado, secciones faltantes…)
+
+3- Solucionar dichos errores
+
+4- Añadir secciones legales.
+
+5- Rediseño de la pagina
+
+6- Nueva comprobación de rendimiento.
+
+7- Cambiar el proyecto a Astro y comprobar el rendimiento, analizar diferencias de resultados.
